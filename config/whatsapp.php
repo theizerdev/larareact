6b5078afa47f15fc611843dd37fe4502518d2fa2
Theizer Dev <theizerdev@gmail.com>
2026-08-04 14:46:18 -0400

    chore: update WhatsApp API key in configuration file

--- config/whatsapp.php
+++ config/whatsapp.php
@@ -2,6 +2,6 @@
 
 return [
     'api_url' => env('WHATSAPP_API_URL', 'http://166.1.85.56:3000'),
-    'api_key' => env('WHATSAPP_API_KEY', 'ac31f0a8-c0ba-41a2-b2ca-1a38b01993e9'),
+    'api_key' => env('WHATSAPP_API_KEY', 'cbd21569-e530-427f-993b-4abcb2a08f81'),
     'timeout' => env('WHATSAPP_TIMEOUT', 30),
 ];
