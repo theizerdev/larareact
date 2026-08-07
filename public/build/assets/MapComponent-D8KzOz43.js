a5cc3c26dd474a9ea1657932107fb9928009d04b
Theizer Dev <theizerdev@gmail.com>
2026-08-07 17:21:38 -0400

    refactor: update build assets and sync manifest for subscription page changes

---
+++ public/build/assets/MapComponent-D8KzOz43.js
@@ -0,0 +1,1 @@
+import{r as e,t}from"./app-Bj33vrUT.js";import{t as n}from"./leaflet-map-DQWg1OfY.js";e();var r=t(),i=({markerPosition:e,zoom:t,onLocationSelected:i})=>(0,r.jsx)(`div`,{className:`w-full h-80 min-h-[320px] rounded-lg overflow-hidden border`,children:(0,r.jsx)(n,{lat:e?e[0]:0,lng:e?e[1]:0,zoom:t,onChange:i,interactive:!0})});export{i as default};
--- resources/js/pages/subscription/expired.tsx
+++ resources/js/pages/subscription/expired.tsx
@@ -146,7 +146,7 @@ export default function SubscriptionExpired({ empresa, plan, planes = [], opcion
     };
 
     return (
-        <div className="p-4 sm:p-6 lg:p-8 max-w-4xl mx-auto space-y-6">
+        <div className="p-4 sm:p-6 lg:p-8 w-full space-y-6">
             <Head title={__('Suscripción Vencida')} />
 
             {/* Header Alerta */}
