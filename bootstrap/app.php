e4974b491e8799e4d480cce9db43f325164306fe
RobertoMarcelino98 <robertomarcelino.mx@gmail.com>
2026-08-18 17:16:53 +0000

    fix: limitar a 10 los assets precargados via Link header
    El header Link combinado (preload de ~50 fuentes + assets de la nueva
    landing) superaba los 8192 bytes que el proxy reverso del host permite
    por header de respuesta, causando 502 Bad Gateway en produccion.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -51,7 +51,7 @@ return Application::configure(basePath: dirname(__DIR__))
             SetLocale::class,
             HandleAppearance::class,
             HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
+            AddLinkHeadersForPreloadedAssets::using(10),
         ]);
 
         $middleware->validateCsrfTokens(except: [
