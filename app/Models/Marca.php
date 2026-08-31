d75041b3b0acdd13d807eacda85c448f1b310163
Theizer Gonzalez <theizerdev@gmail.com>
2026-08-31 20:24:14 +0200

    feat: expand Spanish activity log labels and integrate trait into core models

--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasSpanishActivityLog;
 use App\Traits\Multitenantable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,7 +11,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Marca extends Model
 {
-    use HasFactory, Multitenantable;
+    use HasFactory, Multitenantable, HasSpanishActivityLog;
 
     protected $table = 'marcas';
 
--- app/Models/Servicio.php
+++ app/Models/Servicio.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasSpanishActivityLog;
 use App\Traits\Multitenantable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Servicio extends Model
 {
-    use HasFactory, Multitenantable;
+    use HasFactory, Multitenantable, HasSpanishActivityLog;
 
     protected $table = 'servicios';
 
